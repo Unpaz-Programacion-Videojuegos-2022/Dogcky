0ff0fe93449ccc5ba3b96569efbad96ea72655b9
Adrian Zalazar <papin_1@hotmail.es>
2022-10-28 21:02:19 -0300

    mejoras
    se areglaron algunos problemas y se preparo una demo para la clase

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -8,3 +8,11 @@ func _on_Tutorial_pressed():
 
 func _on_Salir_pressed():
 	get_tree().quit()
+
+
+func _on_Jugar_pressed():
+	get_tree().change_scene("res://Scenes/prueba de movimiento.tscn")
+
+
+func _on_Creditos_pressed():
+	get_tree().change_scene("res://Scenes/creditos.tscn")
