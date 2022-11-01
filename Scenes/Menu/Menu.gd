6650ae0316ad2153031f02b53ab8c4a35883ab9f
Adrian Zalazar <papin_1@hotmail.es>
2022-11-01 11:49:21 -0300

    casi la cago jajaja
    se organizo mejor la ubicacion de las scenas

--- Scenes/Menu/Menu.gd
+++ Scenes/Menu/Menu.gd
@@ -3,16 +3,22 @@ extends Control
 
 
 func _on_Tutorial_pressed():
-	get_tree().change_scene("res://Scenes/Tutorial.tscn")
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Scenes/Tutorial/Tutorial.tscn")
+	
+
+func _on_Jugar_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Scenes/Nivel prueba/prueba de movimiento.tscn")
+	pass # Replace with function body.
+	
+func _on_Creditos_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Scenes/creditos/creditos.tscn")
+	pass # Replace with function body.
+	
 
 
 func _on_Salir_pressed():
 	get_tree().quit()
-
-
-func _on_Jugar_pressed():
-	get_tree().change_scene("res://Scenes/prueba de movimiento.tscn")
-
-
-func _on_Creditos_pressed():
-	get_tree().change_scene("res://Scenes/creditos.tscn")
+	pass # Replace with function body.
--- Scenes/prueba de movimiento.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene("res://Scenes/Menu.tscn")
